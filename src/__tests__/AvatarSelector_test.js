62b727553bb463491e71d6f2f900523f9435497d
evelinasundin <evelinasundin@hotmail.se>
2018-01-19 14:04:03 +0100

    avatarselector.js is 100%

--- src/__tests__/AvatarSelector_test.js
+++ src/__tests__/AvatarSelector_test.js
@@ -1,7 +1,25 @@
 import React from 'react';
-import { render } from 'enzyme';
-import App from '../components/AvatarSelector';
+import { shallow } from 'enzyme';
+import AvatarSelector from '../components/AvatarSelector';
+import esmeralda from '../images/esmeralda.png';
+import zac from '../images/zac.png';
+import morgana from '../images/morgana.png';
 
-test('renders the app', () => {
-  render(<App />);
-});
+it('should set img as zac', ()  => {
+    let currentPersona = 'Zac'; 
+    let wrapper = shallow(<AvatarSelector currentPersona={currentPersona} />);
+    expect(wrapper.find('img').html()).toContain("zac");
+})
+
+it('should set img as esmeralda', ()  => {
+    let currentPersona = 'Esmeralda'; 
+    let wrapper = shallow(<AvatarSelector currentPersona={currentPersona} />);
+    console.log(AvatarSelector.currentPersona);
+    expect(wrapper.find('img').html()).toContain("esmeralda");
+})
+
+it('should set img as morgana', ()  => {
+    const currentPersona = 'Morgana'; 
+    const wrapper = shallow(<AvatarSelector currentPersona={currentPersona} />);
+    expect(wrapper.find('img').html()).toContain("morgana");
+})
