d1f37d58dd0890813dedb6441304923a438ac06e
evelinasundin <evelinasundin@hotmail.se>
2018-01-25 12:23:13 +0100

    test added to bot message

--- src/__tests__/Bot_test/Message_test.js
+++ src/__tests__/Bot_test/Message_test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Message from '../../components/Bot/Message';
 
+describe("message tests", () => {
+
 it('should render message from user with right class', () => {
     const wrapper = mount(<Message bot={false} message="hey you" />); 
     expect(wrapper.find('p .bg-indigo-dark').text()).toEqual("hey you");
@@ -10,4 +12,11 @@ it('should render message from user with right class', () => {
 it('should render message from bot with right class', () => {
     const wrapper = mount(<Message bot message="hey bot" />); 
     expect(wrapper.find('p .bg-white').text()).toEqual("hey bot");
+});
+
+it('should not render message from bot with wrong class', () => {
+    const wrapper = mount(<Message bot message="hey you" />); 
+    expect(wrapper.find('p .bg-indigo-dark')).not.toHaveLength(1);
+});
+
 });
