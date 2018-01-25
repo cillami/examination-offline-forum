af742719f77d5811d98f066fe375a8e1373e9cbc
evelinasundin <evelinasundin@hotmail.se>
2018-01-25 15:10:24 +0100

    message form done

--- src/__tests__/Bot_test/MessageForm_test.js
+++ src/__tests__/Bot_test/MessageForm_test.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import MessageForm from '../../components/Bot/MessageForm';
+import React from "react";
+import { mount } from "enzyme";
+import MessageForm from "../../components/Bot/MessageForm";
 
-it('should update user input ot be output', () => {
-    const onSubmit = jest.fn();
-    const wrapper = mount(<MessageForm onSubmit={onSubmit} />);
-    wrapper.find('[name="userMessage"]').simulate('change', {target:{name:'userMessage', value:'hejhej'}});
-    expect(wrapper.find('input .shadow').prop('value')).toBe("hejhej");
-}); 
+describe("messageform tests", () => {
+
+it("should update user input to be output", () => {
+  const onSubmit = jest.fn();
+  const wrapper = mount(<MessageForm onSubmit={onSubmit} />);
+  wrapper
+    .find('[name="userMessage"]')
+    .simulate("change", { target: { name: "userMessage", value: "hejhej" } });
+  expect(
+    wrapper
+      .find("input")
+      .first()
+      .prop("value")
+  ).toBe("hejhej");
+});
+
+it("test submit", () => {
+  const onSubmit = jest.fn();
+  const wrapper = mount(
+    <MessageForm userMessage={onSubmit} onSubmit={onSubmit} />
+  );
+  wrapper.find("form").simulate("submit", { preventDefault: onSubmit });
+  expect(onSubmit).toBeCalled();
+});
+
+});
