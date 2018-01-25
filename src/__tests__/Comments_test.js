275478c78647d0ea5ad1a019e4df41b900bf0b35
cillami <cillaminami@gmail.com>
2018-01-25 14:41:28 +0100

    comments

--- src/__tests__/Comments_test.js
+++ src/__tests__/Comments_test.js
@@ -16,6 +16,24 @@ it.skip('test', () => {
       ];
 })
 
+it("test to render comments", () => {
+    const fakeComment = [
+      {
+        comment: "My comment",
+        id: "0",
+        author: "Zac",
+        currentPersona: "Zac",
+        date: "1999-12-31"
+      }
+    ];
+  
+    const wrapper = shallow(<Comments postId="1" currentPersona="Zac" author="Zac" />);
+    wrapper.setState({ comments: fakeComment });
+    wrapper.instance().renderCommentList(fakeComment);
+    const findMyComment = wrapper.find("article").first();
+    expect(findMyComment.text()).toContain("My comment");
+  });
+
 
 
 
--- src/__tests__/Posts_test.js
+++ src/__tests__/Posts_test.js
@@ -2,12 +2,11 @@ import React from "react";
 import { shallow, mount } from "enzyme";
 import Posts from "../components/Posts";
 
-it.skip("test to render posts", () => {
-
+it("test to render posts", () => {
   const fakePost = [
     {
-        title: "My title",
-    content: "My post",
+      title: "My title",
+      content: "My post",
       id: "0",
       author: "",
       date: "1999-12-31"
@@ -15,12 +14,8 @@ it.skip("test to render posts", () => {
   ];
 
   const wrapper = shallow(<Posts currentPersona="" author="" />);
-  
   wrapper.setState({ posts: fakePost });
-
   wrapper.instance().renderPostList(fakePost);
-
-  const findMyPost = wrapper.find('article').first();
-
+  const findMyPost = wrapper.find("article").first();
   expect(findMyPost.text()).toContain("My title");
 });
