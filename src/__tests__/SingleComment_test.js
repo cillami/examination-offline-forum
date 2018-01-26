f89a1cb889d0c65d4fd482432c3df43e3459cc20
evelinasundin <evelinasundin@hotmail.se>
2018-01-26 14:50:46 +0100

    indentation fixed single comments

--- src/__tests__/SingleComment_test.js
+++ src/__tests__/SingleComment_test.js
@@ -1,29 +1,53 @@
-import React from 'react';
-import { render, mount } from 'enzyme';
-import SingleComment from '../components/SingleComment';
+import React from "react";
+import { render, mount } from "enzyme";
+import SingleComment from "../components/SingleComment";
 
+// double this check test
+it("button should exist when author and currentpersona is same", () => {
+  const onClick = jest.fn();
+  const wrapper = render(
+    <SingleComment
+      id="5"
+      author="Evelina"
+      onClick={onClick}
+      currentPersona="Evelina"
+      comment="Comment"
+      date="2018-01-05"
+    />
+  );
+  expect(wrapper.find("button")).toBeTruthy();
+});
 
-    // double this check test 
-    it("button should exist when author and currentpersona is same", () => {
-        const onClick = jest.fn();
-        const wrapper = render(<SingleComment id="5" author="Evelina" onClick={onClick} currentPersona="Evelina" comment="Comment" date="2018-01-05" />)
-        expect(wrapper.find('button')).toBeTruthy();
-    })
+describe("test that onlick is being called with right id", () => {
+  it("onlick should have been called with the right id ", () => {
+    const onClick = jest.fn();
+    const wrapper = mount(
+      <SingleComment
+        id="5"
+        author="Evelina"
+        onClick={onClick}
+        currentPersona="Evelina"
+        comment="Comment"
+        date="2018-01-05"
+      />
+    );
+    wrapper.find("button").simulate("click");
+    expect(onClick).toHaveBeenCalledWith("5");
+  });
 
-    describe("test that onlick is being called with right id", () => {
-
-    it('onlick should have been called with the right id ', () => {
-        const onClick = jest.fn();
-        const wrapper = mount(<SingleComment id="5" author="Evelina" onClick={onClick} currentPersona="Evelina" comment="Comment" date="2018-01-05"  />);
-        wrapper.find('button').simulate('click');
-        expect(onClick).toHaveBeenCalledWith('5');
-      });
-
-      it('onlick should not be called when wrong id ', () => {
-        const onClick = jest.fn();
-        const wrapper = mount(<SingleComment id="7" author="Evelina" onClick={onClick} currentPersona="Evelina" comment="Comment" date="2018-01-05"  />);
-        wrapper.find('button').simulate('click');
-        expect(onClick).not.toHaveBeenCalledWith('8');
-      });
-
-    });
+  it("onlick should not be called when wrong id ", () => {
+    const onClick = jest.fn();
+    const wrapper = mount(
+      <SingleComment
+        id="7"
+        author="Evelina"
+        onClick={onClick}
+        currentPersona="Evelina"
+        comment="Comment"
+        date="2018-01-05"
+      />
+    );
+    wrapper.find("button").simulate("click");
+    expect(onClick).not.toHaveBeenCalledWith("8");
+  });
+});
