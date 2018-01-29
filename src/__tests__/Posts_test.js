07248f053cba1148f382c43d9c7cdb5fb36ad267
cillami <cillaminami@gmail.com>
2018-01-29 12:42:39 +0100

    api index.js

--- src/__tests__/Posts_test.js
+++ src/__tests__/Posts_test.js
@@ -2,6 +2,9 @@ import React from "react";
 import { shallow } from "enzyme";
 import Posts from "../components/Posts";
 
+describe("post", ()=>{
+
+ 
 const fakePost = [
   {
     title: "My title",
@@ -15,8 +18,23 @@ const fakePost = [
 it("test to add post and remove post", () => {
   const wrapper = shallow(<Posts currentPersona="Morgana" author="Morgana" />);
   wrapper.setState({ posts: fakePost });
+
   expect(wrapper.state().posts[0].content).toContain("My post");
+
   wrapper.instance().removePost("0");
   // console.log(wrapper.state());
   expect(wrapper.state().posts).toEqual([]);
+
+  
 });
+it("test to add post and remove post", () => {
+  const wrapper = shallow(<Posts currentPersona="Morgana" author="Morgana" />);
+  wrapper.setState({ posts: fakePost });
+
+  expect(wrapper.state().posts[0].content).toContain("My post");
+
+  // wrapper.instance().removePost("7");
+  console.log(wrapper.state().posts[0].content);
+  // expect(wrapper.state().posts[0].content).toContain("My post");
+});
+})
---
+++ src/__tests__/api/index_test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import api from '../../api/';
+
+
+it('test', ()=>{
+    
+    
+})
