000b6fcce0d87537d7a1e41ccceb6d3821f66d85
evelinasundin <evelinasundin@hotmail.se>
2018-01-29 14:44:32 +0100

    remove post from localstorage

--- src/__tests__/api/index_test.js
+++ src/__tests__/api/index_test.js
@@ -8,16 +8,20 @@ describe("post tests", () => {
     localStorage.clear();
   });
 
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
   const fakePosts = [
     {
-      id: "0",
+      id: "1",
       title: "post1",
       content: "content1",
       author: "evelina",
       date: "2018-01-29"
     },
     {
-      id: "1",
+      id: "2",
       title: "post2",
       content: "content2",
       author: "cilla",
@@ -25,10 +29,18 @@ describe("post tests", () => {
     }
   ];
 
-  it('should return a post from localStorage', ()=> {
+  it('should return all post from local storage', ()=> {
     localStorage.setItem('posts', JSON.stringify(fakePosts));
     expect(api.fetchAllPosts()).toEqual(fakePosts);
 });
+
+it('should be able to remove posts from localstorage', ()=>{
+    localStorage.setItem('posts', JSON.stringify(fakePosts));
+    // removes first post in array
+    api.removePost('1')
+    // the only post left is the second post
+    expect(localStorage.getItem('posts')).toContain("post2");
+  });
 
 
 }); // end of description post tests
