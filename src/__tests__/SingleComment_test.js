091a7ebdb43efccf3d3f165ffc09eeb8703f794f
cillami <cillaminami@gmail.com>
2018-01-31 21:18:19 +0100

    bah

--- src/__tests__/SingleComment_test.js
+++ src/__tests__/SingleComment_test.js
@@ -2,24 +2,36 @@ import React from "react";
 import { mount } from "enzyme";
 import SingleComment from "../components/SingleComment";
 
-describe('button test', () => {
+describe("button test", () => {
+  function renderButton(currentPersona, author) {
+    const onClick = jest.fn();
+    const wrapper = mount(
+      <SingleComment
+        id="5"
+        author={author}
+        onClick={onClick}
+        currentPersona={currentPersona}
+        comment="Comment"
+        date="2018-01-05"
+      />
+    );
+    return wrapper;
+  }
+  it("button should not exist when author and currentpersona is not the same", () => {
+    expect(
+      renderButton("Evelina", "Cilla")
+        .find("button")
+        .exists()
+    ).toBeFalsy();
+  });
 
-  function renderButton(currentPersona, author){
-  const onClick = jest.fn();
-  const wrapper = mount(
-    <SingleComment
-      id="5" author={author} onClick={onClick} currentPersona={currentPersona} comment="Comment" date="2018-01-05"
-    />
-  );
-  return wrapper
-}
-it("button should not exist when author and currentpersona is not the same", () => {
-  expect(renderButton("Evelina", "Cilla").find("button").exists()).toBeFalsy();
-});
-
-it("button should exist when author and currentpersona is the same", () =>{
-  expect(renderButton("Cilla", "Cilla").find("button").exists()).toBeTruthy();
-})
+  it("button should exist when author and currentpersona is the same", () => {
+    expect(
+      renderButton("Cilla", "Cilla")
+        .find("button")
+        .exists()
+    ).toBeTruthy();
+  });
 });
 
 describe("test that onlick is being called with right id", () => {
@@ -27,7 +39,13 @@ describe("test that onlick is being called with right id", () => {
     const onClick = jest.fn();
     const wrapper = mount(
       <SingleComment
-        id="5" author="Evelina" onClick={onClick} currentPersona="Evelina" comment="Comment" date="2018-01-05" />
+        id="5"
+        author="Evelina"
+        onClick={onClick}
+        currentPersona="Evelina"
+        comment="Comment"
+        date="2018-01-05"
+      />
     );
     wrapper.find("button").simulate("click");
     expect(onClick).toHaveBeenCalledWith("5");
@@ -37,7 +55,12 @@ describe("test that onlick is being called with right id", () => {
     const onClick = jest.fn();
     const wrapper = mount(
       <SingleComment
-        id="7" author="Evelina" onClick={onClick} currentPersona="Evelina" comment="Comment" date="2018-01-05"
+        id="7"
+        author="Evelina"
+        onClick={onClick}
+        currentPersona="Evelina"
+        comment="Comment"
+        date="2018-01-05"
       />
     );
     wrapper.find("button").simulate("click");
