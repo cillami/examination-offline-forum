e058556364a27496c0c1b3b28ef3a7116221ee2b
cillami <cillaminami@gmail.com>
2018-02-01 14:01:12 +0100

    hahej

--- src/__tests__/Comments_test.js
+++ src/__tests__/Comments_test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { mount } from "enzyme";
 import Comments from "../components/Comments";
+import * as api from "../api/";
 
 describe("test add and remove comment", () => {
-
   afterEach(() => {
     localStorage.clear();
   });
@@ -25,27 +25,26 @@ describe("test add and remove comment", () => {
     );
     localStorage.setItem("comments", JSON.stringify(fejkComment));
     wrapper.instance().setCommentsFromLocalStorage();
-    // expect(localStorage.getItem).toHaveBeenCalled();
+    expect(localStorage.getItem).toHaveBeenCalled();
+
+    const fetchEm = api.fetchAllCommments();
+    expect(fetchEm).toHaveLength(1);
     expect(wrapper.state().comments).toHaveLength(1);
   });
 
-
-  
   it("Test remove a comment", () => {
     const wrapper = mount(
       <Comments postId="the_hundreds" currentPersona="Zac" author="Zac" />
     );
-
     localStorage.setItem("comments", JSON.stringify(fejkComment));
     wrapper.instance().setCommentsFromLocalStorage();
 
-    // const fetchEm = api.fetchAllCommments();
-    // expect(fetchEm).toHaveLength(1);
-    // console.log(fetchEm)
-
+    const fetchEm = api.fetchAllCommments();
+    expect(fetchEm).toHaveLength(1);
     expect(wrapper.state().comments).toHaveLength(1);
-    wrapper.instance().removeComment("Haru pass eller id?");
-    expect(wrapper.state().comments).toEqual([]);
+    
+    api.removeComment = jest.fn();
+    wrapper.instance().removeComment();
+    expect(api.removeComment).toBeCalled();
   });
-
 });
